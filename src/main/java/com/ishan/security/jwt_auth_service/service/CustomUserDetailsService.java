9d3953fa9494b1956d8e853e9883fd09e5e210d5
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-12-13 21:59:14 +0530

    chore: move customUserService to service package

--- src/main/java/com/ishan/security/jwt_auth_service/service/CustomUserDetailsService.java
+++ src/main/java/com/ishan/security/jwt_auth_service/service/CustomUserDetailsService.java
@@ -1,4 +1,4 @@
-package com.ishan.security.jwt_auth_service.service.auth;
+package com.ishan.security.jwt_auth_service.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
