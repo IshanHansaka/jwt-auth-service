d7e9d9ae46bf6ab46b48c776b0a307eba56b5d4b
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-12-13 22:02:19 +0530

    chore: change health check endpoint

--- src/main/java/com/ishan/security/jwt_auth_service/controller/HealthCheckController.java
+++ src/main/java/com/ishan/security/jwt_auth_service/controller/HealthCheckController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1/health-check")
+@SecurityRequirement(name = "bearerAuth")
 public class HealthCheckController {
 
     @GetMapping
