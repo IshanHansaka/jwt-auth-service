66f5414522fbd0d9a8e5fcf8aa084e5e1632d655
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-12-14 20:50:13 +0530

    feat: create user mapper

---
+++ src/main/java/com/ishan/security/jwt_auth_service/util/UserMapper.java
@@ -0,0 +1,25 @@
+package com.ishan.security.jwt_auth_service.util;
+
+import org.springframework.stereotype.Component;
+
+import com.ishan.security.jwt_auth_service.dto.user.UserRegisterDTO;
+import com.ishan.security.jwt_auth_service.model.User;
+
+import lombok.NonNull;
+
+@Component
+public class UserMapper {
+
+    @NonNull
+    public User toEntity(@NonNull UserRegisterDTO dto, @NonNull String encodedPassword) {
+        return User.builder()
+                .email(normalizeEmail(dto.getEmail()))
+                .name(dto.getName())
+                .password(encodedPassword)
+                .build();
+    }
+
+    private String normalizeEmail(String email) {
+        return email.trim().toLowerCase();
+    }
+}
