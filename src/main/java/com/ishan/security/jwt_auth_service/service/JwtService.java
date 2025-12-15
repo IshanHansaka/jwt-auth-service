2e258b317c66de13b8d98aafbcd724a34f8dfb35
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-12-15 12:36:17 +0530

    feat: add methods to verify tokens

--- src/main/java/com/ishan/security/jwt_auth_service/service/JwtService.java
+++ src/main/java/com/ishan/security/jwt_auth_service/service/JwtService.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Function;
 
 import javax.crypto.SecretKey;
 
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -89,6 +92,58 @@ public class JwtService {
         String roleWithoutPrefix = role.startsWith("ROLE_") ? role.substring(5) : role;
 
         return roleWithoutPrefix;
+    }
+
+    /**
+     * Validate an access token
+     */
+    public boolean validateAccessToken(String token, UserDetails userDetails) {
+
+        final String username = extractUsername(token);
+        final String type = extractClaim(token, claims -> claims.get("token_type", String.class));
+        return username.equals(userDetails.getUsername())
+                && !isTokenExpired(token)
+                && "access".equals(type);
+    }
+
+    /**
+     * Validate a refresh token
+     */
+    public boolean validateRefreshToken(String refreshToken, UserDetails userDetails) {
+        final String username = extractUsername(refreshToken);
+        final String type = extractClaim(refreshToken, claims -> claims.get("token_type", String.class));
+        return username.equals(userDetails.getUsername())
+                && !isTokenExpired(refreshToken)
+                && "refresh".equals(type);
+    }
+
+    public String extractUsername(String token) throws JwtException {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public String extractRole(String token) {
+        return extractClaim(token, claims -> claims.get("role", String.class));
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     public long getAccessTokenDurationMs() {
