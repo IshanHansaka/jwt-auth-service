6ed69cb9ffce840d5f19351454ae8002c5b0be86
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-12-17 17:02:36 +0530

    feat: add password rest token cleanup job

--- src/main/java/com/ishan/security/jwt_auth_service/job/TokenCleanupJob.java
+++ src/main/java/com/ishan/security/jwt_auth_service/job/TokenCleanupJob.java
@@ -4,6 +4,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.ishan.security.jwt_auth_service.repository.EmailVerificationTokenRepository;
+import com.ishan.security.jwt_auth_service.repository.PasswordResetTokenRepository;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TokenCleanupJob {
 
-    private final EmailVerificationTokenRepository tokenRepository;
-
+    private final EmailVerificationTokenRepository emailVerificationTokenRepository;
+    private final PasswordResetTokenRepository passwordResetTokenRepository;
     /**
      * Cleanup expired email verification tokens
      *
@@ -24,7 +25,19 @@ public class TokenCleanupJob {
     @Transactional
     @Scheduled(cron = "0 0 * * * *")
     public void cleanupExpiredEmailVerificationTokens() {
-        int deletedCount = tokenRepository.deleteExpired();
+        int deletedCount = emailVerificationTokenRepository.deleteExpired();
         log.info("Expired email verification tokens cleaned up: {}", deletedCount);
+    }
+
+    /**
+     * Cleanup expired password reset tokens
+     *
+     * Runs every 1 hour
+     */
+    @Scheduled(cron = "0 0 * * * *")
+    @Transactional
+    public void cleanupPasswordResetTokens() {
+        int count = passwordResetTokenRepository.deleteExpired();
+        log.info("Password reset tokens cleaned up: {}", count);
     }
 }
