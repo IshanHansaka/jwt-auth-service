e9fc4d8d7b9c329f22829c31e452d9eea45ff380
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-12-17 17:02:03 +0530

    feat: add new request dtos

---
+++ src/main/java/com/ishan/security/jwt_auth_service/dto/request/ResetPasswordRequestDTO.java
@@ -0,0 +1,24 @@
+package com.ishan.security.jwt_auth_service.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResetPasswordRequestDTO {
+
+    @NotBlank(message = "Token is required")
+    private String token;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).*$", message = "Password must contain uppercase, lowercase, number, and special character")
+    @Schema(example = "string")
+    private String newPassword;
+}
